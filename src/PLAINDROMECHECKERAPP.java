b78070e7f93ee678976a6e1a6f67494ec66e454d
Bharat2006-KBK <bk0815@srmist.edu.in>
2026-02-18 15:13:13 +0530

    UC1- ADDED PRINT WELCOME MESSAGE

--- src/PLAINDROMECHECKERAPP.java
+++ src/PLAINDROMECHECKERAPP.java
@@ -1,4 +1,12 @@
-
-
 public class PLAINDROMECHECKERAPP {
+    public static void main(String[] args){
+        // Display welcome message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+        // Display version
+        System.out.println("Version : 1.0");
+
+        // Display system initialization message
+        System.out.println("System initialized successfully.");
+    }
 }
